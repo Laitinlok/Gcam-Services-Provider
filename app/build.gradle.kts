14b4451b9a9b26973371a2c8bdcbef7d457db0df
Laitinlok <63400461+Laitinlok@users.noreply.github.com>
2026-07-14 19:54:05 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
             dimension = "default"
             applicationId = "com.google.android.googlequicksearchbox"
             versionCode = 301776834
-            versionName = "1.6.1"
+            versionName = "17.40.19"
         }
     }
 
